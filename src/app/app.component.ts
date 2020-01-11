b0ae6d2074650c8e300117c83968f95915226b8d
Norman Witte III <norm.witte@gmail.com>
2020-01-11 00:50:33 -0500

    Send height to parent window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from './user.service';
+import ResizeObserver from 'resize-observer-polyfill';
 
 @Component({
     selector: 'app-root',
@@ -8,6 +9,7 @@ import { UserService } from './user.service';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    height: number;
 
     constructor(
         private userService: UserService,
@@ -18,6 +20,7 @@ export class AppComponent {
         } else {
             (window as any).attachEvent('onmessage', this.receiveMessage.bind(this));
         }
+        this.elementObserver();
     }
 
     receiveMessage(event) {
@@ -27,6 +30,24 @@ export class AppComponent {
             if (data.token) { this.userService.jwtToken = data.token; }
             if (data.route) { this.router.navigate([data.route]); }
         } catch (err) {}
+    }
+
+    /**
+     * this will bind resize observer to the target element
+     */
+    elementObserver() {
+        const ro = new ResizeObserver(entries => {
+            for (const entry of entries) {
+                if (this.height === entry.contentRect.height) {
+                    return;
+                }
+                this.height = entry.contentRect.height;
+                window.parent.postMessage({ height: entry.contentRect.height } , '*');
+            }
+        });
+
+        // Element for which to observe height and width
+        ro.observe(document.body);
     }
 
 }
